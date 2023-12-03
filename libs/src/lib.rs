d5a360dd63b4bc9c4a440786f74807b1abd6c38a
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-04 05:50:22 +0700

    refactor: extract records from cli/service + extract handlers

---
+++ libs/src/lib.rs
@@ -0,0 +1,19 @@
+use strum_macros::Display;
+
+pub mod record;
+
+#[derive(Debug, Display)]
+pub enum UserRecordRedisJsonPath {
+    #[strum(serialize = "$")]
+    Root,
+    #[strum(serialize = "$.task_history")]
+    TaskHistory,
+    #[strum(serialize = "$.current_task")]
+    CurrentTask,
+}
+
+#[derive(Debug, Display)]
+pub enum OperatingRedisKey {
+    #[strum(serialize = "current_id")]
+    CurrentId,
+}
---
+++ libs/src/record.rs
@@ -0,0 +1,123 @@
+use chrono::NaiveDateTime;
+use redis::FromRedisValue;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum TaskState {
+    Begin,
+    Break,
+    Back,
+    End,
+    Idle,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Task {
+    pub name: String,
+    pub state: TaskState,
+    pub begin_time: NaiveDateTime,
+    pub end_time: NaiveDateTime,
+    pub duration: i64,
+}
+
+impl Default for Task {
+    fn default() -> Self {
+        Task {
+            name: String::new(),
+            state: TaskState::Begin,
+            begin_time: chrono::NaiveDateTime::from_timestamp_opt(0, 0).unwrap(),
+            end_time: chrono::NaiveDateTime::from_timestamp_opt(0, 0).unwrap(),
+            duration: 0,
+        }
+    }
+}
+
+impl Task {
+    pub fn placeholder(name: &str, state: TaskState) -> Self {
+        Task {
+            name: name.to_string(),
+            state,
+            ..Task::default()
+        }
+    }
+
+    pub fn generate_begin_task(name: String) -> Self {
+        Task {
+            name,
+            state: TaskState::Begin,
+            ..Task::default()
+        }
+    }
+
+    pub fn generate_break_task(latest_task: &Task) -> Self {
+        let duration = Task::calculate_duration(&latest_task);
+        Task {
+            name: latest_task.name.clone(),
+            state: TaskState::Break,
+            duration,
+            end_time: chrono::offset::Local::now().naive_local(),
+            ..*latest_task
+        }
+    }
+
+    fn generate_back_task(latest_task: &Task) -> Self {
+        Task {
+            name: latest_task.name.clone(),
+            state: TaskState::Back,
+            begin_time: Task::default().begin_time,
+            ..*latest_task
+        }
+    }
+
+    pub fn generate_done_task(latest_task: &Task) -> Self {
+        if latest_task.state == TaskState::Break {
+            Task {
+                name: latest_task.name.clone(),
+                state: TaskState::End,
+                ..*latest_task
+            }
+        } else if latest_task.state == TaskState::Back {
+            let duration = Task::calculate_duration(&latest_task) + latest_task.duration;
+            Task {
+                name: latest_task.name.clone(),
+                state: TaskState::End,
+                duration,
+                begin_time: latest_task.begin_time,
+                ..Task::default()
+            }
+        } else {
+            let duration = Task::calculate_duration(&latest_task);
+            Task {
+                name: latest_task.name.clone(),
+                state: TaskState::End,
+                duration,
+                ..Task::default()
+            }
+        }
+    }
+
+    fn calculate_duration(&self) -> i64 {
+        let duration = chrono::offset::Local::now().naive_local() - self.begin_time;
+        duration.num_seconds()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserRecord {
+    pub id: i32,
+    pub user_name: String,
+    pub task_history: Vec<Task>,
+    pub current_task: Task,
+}
+
+impl FromRedisValue for UserRecord {
+    fn from_redis_value(v: &redis::Value) -> redis::RedisResult<UserRecord> {
+        match *v {
+            redis::Value::Data(ref bytes) => {
+                let user_data: UserRecord = serde_json::from_slice(bytes)?;
+                Ok(user_data)
+            }
+            _ => Err((redis::ErrorKind::TypeError, "Invalid type").into()),
+        }
+    }
+}
